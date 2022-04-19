a769d8f9cd3c6a7eae93cf13b8ac085539a80a19
Landon Phillips <landonphillips@Landons-MBP.attlocal.net>
2022-04-18 21:43:36 -0500

    changes first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-
+import React, { useReducer } from 'react';
+import reducer, { initialState } from "./../reducers";
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
 function App() {
-  return (
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+  console.log(state)
+   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
         <a className="navbar-brand" href="#"> Reducer Challenge</a>
@@ -16,10 +18,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation}</span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
